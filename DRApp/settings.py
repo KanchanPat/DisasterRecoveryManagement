003c188ee015520a10f8a70d1a5092a2f7156d01
patil <kanchan.rade@gmail.com>
2019-03-29 15:52:46 -0400

    ``

--- DRApp/settings.py
+++ DRApp/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'DRApp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+from decouple import config,Csv
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : config('NAME'),
+        'USER' : config('USER'),
+        'PASSWORD' : config('PASSWORD'),
+
     }
 }
 
--- DRApp/urls.py
+++ DRApp/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('login/',LoginView.as_view(),name='login'),
+    # path('logout/',LogoutView.as_view(),name='logout'),
 ]
