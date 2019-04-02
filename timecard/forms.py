2ef3acaa4b67e69f1bb1dab6fb6eb2a03dc0fe49
patil <kanchan.rade@gmail.com>
2019-04-02 10:11:02 -0400

    ``

---
+++ timecard/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+
+from timecard.models import Timecard, Job, JobEntry, MachineEntry
+
+
+class CreateTimeCardForm(forms.ModelForm):
+    class Meta:
+        model = Timecard
+        fields = ['site_code','contractor_name','date']
+        # exclude = []
+
+class JobEntryFom(forms.ModelForm):
+    class Meta:
+        model = JobEntry
+        exclude = []
+        def cleaned_jobentry(self):
+            data = self.cleaned_data['job_code','hours_worked','total']
+#
+# class MachineEntryForm(forms.ModelForm):
+#     class Meta:
+#         model = MachineEntry
+#         exclude = []
+#         def cleaned_jobentry(self):
+#             data = self.cleaned_data['machinecode','hoursused','machinetotal']
+#             return data
+#
+# class LaborEntryForm(forms.Form):
+#     # # model = Timecard_Details
+#     # class Meta:
+#     #     model = Job
+#     #     jobs = forms.ModelChoiceField(queryset=Job.objects.all())
+#     #     fields = ('job_code',)
+#     #     widgets = {
+#     #         'job': jobs,
+#     #
+#     #     }
+#
+#     jobcode = forms.ChoiceField(choices=[(choice.pk,choice) for choice in Job.objects.all()])
+#     hours = forms.IntegerField()
+#     total = forms.IntegerField()
+#
